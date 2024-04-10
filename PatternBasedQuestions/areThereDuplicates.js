07ffdb81b5dc2527776ffe24e7d9b033a539a787
tejasva-087 <khandelwaltejasva@gmail.com>
2024-04-10 19:22:08 +0530

    Did some more problems on pattern based questions

--- PatternBasedQuestions/areThereDuplicates.js
+++ PatternBasedQuestions/areThereDuplicates.js
@@ -1,6 +1,31 @@
+// Frequency Counter / Multiple Pointers - areThereDuplicates
+// Implement a function called, areThereDuplicates which accepts a variable number of arguments, and checks whether there are any duplicates among the arguments passed in.  You can solve this using the frequency counter pattern OR the multiple pointers pattern.
+
+// Examples:
+
+// areThereDuplicates(1, 2, 3) // false
+// areThereDuplicates(1, 2, 2) // true
+// areThereDuplicates('a', 'b', 'c', 'a') // true
+// Restrictions:
+
+// Time - O(n)
+
+// Space - O(n)
+
+// Bonus:
+
+// Time - O(n log n)
+
+// Space - O(1)
+
 // My solution 1 (works best for sorted arrays)
 // const areThereDuplicates = function (...arr) {
-//   arr.sort(); // (But this works for sorted arrays better)
+//   // arr.sort(); // (But this works for sorted arrays better)
+//   args.sort((a, b) => {
+//     if (a < b) return -1;
+//     if (a > b) return 1;
+//     return 0;
+//   });
 //   let pointer = 0;
 //   while (pointer < arr.length - 1) {
 //     if (arr[pointer] === arr[pointer + 1]) {
@@ -26,17 +51,24 @@
 //   return false;
 // };
 
+// [1, 2, 3, 4, 5, 6]
+
 // using multiple pointers
 const areThereDuplicates = function () {
   let pointer1 = 0;
   let pointer2 = 1;
   while (pointer1 < arguments.length - 1) {
-    if (arguments[pointer1] === arguments[pointer2]) {
-      return true;
+    const pointer1val = arguments[pointer1];
+    const pointer2val = arguments[pointer2];
+    if (pointer1val === pointer2val) return true;
+    else if (pointer2 === arguments.length) {
+      pointer1++;
+      pointer2 = pointer1 + 1;
     } else {
       pointer2++;
     }
   }
+  return false;
 };
 
 console.log(areThereDuplicates(1, 2, 3));
--- PatternBasedQuestions/sameFrequency.js
+++ PatternBasedQuestions/sameFrequency.js
@@ -1,3 +1,16 @@
+// Write a function called sameFrequency. Given two positive integers, find out if the two numbers have the same frequency of digits.
+
+// Your solution MUST have the following complexities:
+
+// Time: O(N)
+
+// Sample Input:
+
+// sameFrequency(182,281) // true
+// sameFrequency(34,14) // false
+// sameFrequency(3589578, 5879385) // true
+// sameFrequency(22,222) // false
+
 const sameFrequency = function (n1, n2) {
   n1 = String(n1);
   n2 = String(n2);
