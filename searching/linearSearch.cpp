77ebe99f20bc5b6fce6c1ff71d214ed728b48d66
tejasva-087 <khandelwaltejasva@gmail.com>
2024-08-10 00:13:16 +0530

    implemented linear search using recursion in cpp

--- searching/linearSearch.cpp
+++ searching/linearSearch.cpp
@@ -1,7 +1,7 @@
 # include <iostream>
 using namespace std;
 
-int linearSearch(int arr[], int size, int val) {
+int linearSearchForLoop(int arr[], int size, int val) {
 	for (int i = 0; i < size; i++) {
 		if (arr[i] == val) {
 			return i;
@@ -11,13 +11,19 @@ int linearSearch(int arr[], int size, int val) {
 	return -1;
 }
 
-int linearSearch(int arr[], int ptr, int val) {
-	if ()
+int linearSearch(int arr[], int size, int val, int ptr = 0) {
+	if (ptr == size - 1) {
+    return -1;
+  }
+  if (arr[ptr] == val) {
+    return ptr;
+  }
+  return linearSearch(arr, size, val, ++ptr);
 }
 
 int main() {
 	int arr[] = {1, 3, 1, 5, 6, 12, 32, 24, 1};
 	int size = sizeof(arr) / sizeof(arr[0]);
-	cout << linearSearch(arr, size,12) << endl;
+	cout << linearSearch(arr, size,122) << endl;
 	return 0;
 }
